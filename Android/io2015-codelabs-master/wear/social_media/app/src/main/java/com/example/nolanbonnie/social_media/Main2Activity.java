989048fae9fe4e8d02441ff9962a707e225de715
NolanRBonnie <nbonnie@uci.edu>
2019-02-16 14:55:23 -0800

    Milestone: Data Grabbing

--- Android/io2015-codelabs-master/wear/social_media/app/src/main/java/com/example/nolanbonnie/social_media/Main2Activity.java
+++ Android/io2015-codelabs-master/wear/social_media/app/src/main/java/com/example/nolanbonnie/social_media/Main2Activity.java
@@ -1,12 +1,28 @@
 package com.example.nolanbonnie.social_media;
 
+import android.app.ActivityManager;
+import android.app.AppOpsManager;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.icu.text.Collator;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.content.Intent;
+
+import java.util.Calendar;
+import java.util.List;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -43,6 +59,54 @@ public class Main2Activity extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setSelectedItemId(R.id.navigation_dashboard);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
 
+        if (!isAccessGranted()) {
+            Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
+            startActivity(intent);
+        }
+
+        final UsageStatsManager usageStatsManager=(UsageStatsManager)this.getSystemService(Context.USAGE_STATS_SERVICE);// Context.USAGE_STATS_SERVICE);
+
+        Calendar beginCal = Calendar.getInstance();
+        beginCal.set(Calendar.DATE, 10);
+        beginCal.set(Calendar.MONTH, 2);
+        beginCal.set(Calendar.YEAR, 2019);
+
+        Calendar endCal = Calendar.getInstance();
+        endCal.set(Calendar.DATE, 16);
+        endCal.set(Calendar.MONTH, 2);
+        endCal.set(Calendar.YEAR, 2019);
+
+        final List<UsageStats> queryUsageStats=usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_BEST,
+                System.currentTimeMillis() - 10000000, System.currentTimeMillis());
+
+        for (UsageStats usageStats : queryUsageStats) {
+            if (usageStats.getTotalTimeInForeground() > 0) {
+                TextView textbox = (TextView) findViewById(R.id.editText);
+                textbox.setMovementMethod(new ScrollingMovementMethod());
+                textbox.setText(textbox.getText() + "\n" + usageStats.getPackageName() + ": " + usageStats.getTotalTimeInForeground(), TextView.BufferType.SPANNABLE);
+                Log.i("Stats", usageStats.getPackageName() + " " + usageStats.getTotalTimeInForeground());
+            }
+
+        }
+
+
+
+    }
+    private boolean isAccessGranted() {
+        try {
+            PackageManager packageManager = getPackageManager();
+            ApplicationInfo applicationInfo = packageManager.getApplicationInfo(getPackageName(), 0);
+            AppOpsManager appOpsManager = (AppOpsManager) getSystemService(Context.APP_OPS_SERVICE);
+            int mode = 0;
+            if (android.os.Build.VERSION.SDK_INT > android.os.Build.VERSION_CODES.KITKAT) {
+                mode = appOpsManager.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS,
+                        applicationInfo.uid, applicationInfo.packageName);
+            }
+            return (mode == AppOpsManager.MODE_ALLOWED);
+
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
 }
