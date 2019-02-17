80546a2d579746a6458a8af007113237b3fb731a
NolanRBonnie <nbonnie@uci.edu>
2019-02-16 22:09:59 -0800

    7 day data

--- Android/io2015-codelabs-master/wear/social_media/app/src/main/java/com/example/nolanbonnie/social_media/Main2Activity.java
+++ Android/io2015-codelabs-master/wear/social_media/app/src/main/java/com/example/nolanbonnie/social_media/Main2Activity.java
@@ -65,7 +65,7 @@ public class Main2Activity extends AppCompatActivity {
             startActivity(intent);
         }
 
-        final UsageStatsManager usageStatsManager=(UsageStatsManager)this.getSystemService(Context.USAGE_STATS_SERVICE);
+        final UsageStatsManager usageStatsManager = (UsageStatsManager) this.getSystemService(Context.USAGE_STATS_SERVICE);
         // Context.USAGE_STATS_SERVICE);
 
         Calendar beginCal = Calendar.getInstance();
@@ -76,72 +76,301 @@ public class Main2Activity extends AppCompatActivity {
         Calendar endCal = Calendar.getInstance();
         endCal.set(Calendar.DATE, 16);
         endCal.set(Calendar.MONTH, 2);
-        endCal.set(Calendar.YEAR, 2019);
+        endCal.set(Calendar.YEAR, 2020);
 
-        final List<UsageStats> queryUsageStats=usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_BEST,
-                System.currentTimeMillis() - (86400000*7), System.currentTimeMillis()); //1 week data pull
+        long time = System.currentTimeMillis();
+        long day = 86400000; //in ms
 
-        HashMap<String, Long> App_Data_Week = new HashMap<String, Long>();
+        final List<UsageStats> queryUsageStats7 = usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY,
+                time - (day * 7), time - (day * 6)); //1 week data pull
+        final List<UsageStats> queryUsageStats6 = usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY,
+                time - (day * 6), time - (day * 5));
+        final List<UsageStats> queryUsageStats5 = usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY,
+                time - (day * 5), time - (day * 4));
+        final List<UsageStats> queryUsageStats4 = usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY,
+                time - (day * 4), time - (day * 3));
+        final List<UsageStats> queryUsageStats3 = usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY,
+                time - (day * 3), time - (day * 2));
+        final List<UsageStats> queryUsageStats2 = usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY,
+                time - (day * 2), time - (day));
+        final List<UsageStats> queryUsageStats1 = usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY,
+                time - (day), time);
+
+
+        //Log.e("query_stats", queryUsageStats2.toString());
+        //Log.e("query_stats", queryUsageStats1.toString());
+
 
         String[] strs = {"instagram", "snapchat", "facebook", "twitter", "reddit", "pinterest",
-                "linkedin", "tumblr", "flickr", "periscope", "youtube","contacts", "vending", "frontpage", ""};
-
+                "linkedin", "tumblr", "flickr", "periscope", "youtube", "contacts", "vending", "frontpage"};
         List<String> media_sites = Arrays.asList(strs);
 
-        //NEED TO CHECK THESE ID's ARE CORRECT
 
-        for (UsageStats usageStats : queryUsageStats)
+        HashMap<String, Long> App_Data_Day1 = new HashMap<String, Long>();
+        for (UsageStats usageStats : queryUsageStats1)
+
             if (usageStats.getTotalTimeInForeground() > 60000) {  // 60000ms, = 1 minute
                 String app_parts = usageStats.getPackageName();
                 String[] split_name = app_parts.split("\\.");
-
-                if (!split_name[0].matches("com")){
-                    Log.e("qwerty", Arrays.toString(split_name));
-                    Log.e("qwerty", split_name[0].getClass().getName());
-                    //Log.d("qwerty", split_name[split_name.length-1]);
-                   // Log.d("qwerty", )
+                if (!split_name[0].matches("com")) {
                     continue;
-                    //Needs to skip to next for loop iteration
                 }
-
                 String app_name = null;
                 try {
                     app_name = split_name[split_name.length - 1];
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-
-                App_Data_Week.put(app_name, (usageStats.getTotalTimeInForeground() / (1000 * 60)));
-
-
-
+                App_Data_Day1.put(app_name, (usageStats.getTotalTimeInForeground() / (1000 * 60)));
                 TextView textbox = (TextView) findViewById(R.id.editText);
                 textbox.setMovementMethod(new ScrollingMovementMethod());
-
                 double y = (double) usageStats.getTotalTimeInForeground();
                 DecimalFormat df = new DecimalFormat("#.##");
-                // Log.e("long_int", "value" + (y/2));
-
-                if((usageStats.getTotalTimeInForeground() / (1000 * 60)) < 60 ) {
+                Log.e("long_int", "value" + (y / 2));
+                if ((usageStats.getTotalTimeInForeground() / (1000 * 60)) < 60) {
                     textbox.setText(textbox.getText() + "\n" + app_name + ": "
                             + df.format(y / (1000 * 60)) + " Minutes", TextView.BufferType.SPANNABLE);
                 } else {
                     textbox.setText(textbox.getText() + "\n" + app_name + ": "
                             + df.format(y / (1000 * 60 * 60)) + " Hours", TextView.BufferType.SPANNABLE);
                 }
-
                 Log.i("Stats", usageStats.getPackageName() + " " + usageStats.getTotalTimeInForeground());
-
-
             }
-            Log.e("hash_data", App_Data_Week.toString());
-//            for (String i : App_Data_Week.keySet()) {
-//                if (!media_sites.contains(i)){
-//                    App_Data_Week.remove(i);
-//                }
-//            }
-            App_Data_Week.keySet().retainAll(media_sites);
-            Log.e("hash_data", App_Data_Week.toString());
+
+        Log.e("hash_data", App_Data_Day1.toString());
+        App_Data_Day1.keySet().retainAll(media_sites);      // ! parse out non-social media titles
+        Log.e("hash_data", App_Data_Day1.toString());
+
+        TextView textbox = (TextView) findViewById(R.id.editText);
+        textbox.setMovementMethod(new ScrollingMovementMethod());
+        textbox.setText(textbox.getText() + "\n\nDAY 2\n\n", TextView.BufferType.SPANNABLE);
+
+        HashMap<String, Long> App_Data_Day2 = new HashMap<String, Long>();
+        for (UsageStats usageStats : queryUsageStats2)
+
+            if (usageStats.getTotalTimeInForeground() > 60000) {  // 60000ms, = 1 minute
+                String app_parts = usageStats.getPackageName();
+                String[] split_name = app_parts.split("\\.");
+                if (!split_name[0].matches("com")) {
+                    continue;
+                }
+                String app_name = null;
+                try {
+                    app_name = split_name[split_name.length - 1];
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                App_Data_Day2.put(app_name, (usageStats.getTotalTimeInForeground() / (1000 * 60)));
+                //TextView textbox = (TextView) findViewById(R.id.editText);
+                textbox.setMovementMethod(new ScrollingMovementMethod());
+                double y = (double) usageStats.getTotalTimeInForeground();
+                DecimalFormat df = new DecimalFormat("#.##");
+                Log.e("long_int", "value" + (y / 2));
+                if ((usageStats.getTotalTimeInForeground() / (1000 * 60)) < 60) {
+                    textbox.setText(textbox.getText() + "\n" + app_name + ": "
+                            + df.format(y / (1000 * 60)) + " Minutes", TextView.BufferType.SPANNABLE);
+                } else {
+                    textbox.setText(textbox.getText() + "\n" + app_name + ": "
+                            + df.format(y / (1000 * 60 * 60)) + " Hours", TextView.BufferType.SPANNABLE);
+                }
+                Log.i("Stats", usageStats.getPackageName() + " " + usageStats.getTotalTimeInForeground());
+            }
+
+        Log.e("hash_data", App_Data_Day2.toString());
+        App_Data_Day2.keySet().retainAll(media_sites);      // ! parse out non-social media titles
+        Log.e("hash_data", App_Data_Day2.toString());
+
+        textbox.setMovementMethod(new ScrollingMovementMethod());
+        textbox.setText(textbox.getText() + "\n\nDAY 3\n\n", TextView.BufferType.SPANNABLE);
+
+        HashMap<String, Long> App_Data_Day3 = new HashMap<String, Long>();
+        for (UsageStats usageStats : queryUsageStats3)
+
+            if (usageStats.getTotalTimeInForeground() > 60000) {  // 60000ms, = 1 minute
+                String app_parts = usageStats.getPackageName();
+                String[] split_name = app_parts.split("\\.");
+                if (!split_name[0].matches("com")) {
+                    continue;
+                }
+                String app_name = null;
+                try {
+                    app_name = split_name[split_name.length - 1];
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                App_Data_Day3.put(app_name, (usageStats.getTotalTimeInForeground() / (1000 * 60)));
+                //TextView textbox = (TextView) findViewById(R.id.editText);
+                textbox.setMovementMethod(new ScrollingMovementMethod());
+                double y = (double) usageStats.getTotalTimeInForeground();
+                DecimalFormat df = new DecimalFormat("#.##");
+                Log.e("long_int", "value" + (y / 2));
+                if ((usageStats.getTotalTimeInForeground() / (1000 * 60)) < 60) {
+                    textbox.setText(textbox.getText() + "\n" + app_name + ": "
+                            + df.format(y / (1000 * 60)) + " Minutes", TextView.BufferType.SPANNABLE);
+                } else {
+                    textbox.setText(textbox.getText() + "\n" + app_name + ": "
+                            + df.format(y / (1000 * 60 * 60)) + " Hours", TextView.BufferType.SPANNABLE);
+                }
+                Log.i("Stats", usageStats.getPackageName() + " " + usageStats.getTotalTimeInForeground());
+            }
+
+        Log.e("hash_data", App_Data_Day3.toString());
+        App_Data_Day3.keySet().retainAll(media_sites);      // ! parse out non-social media titles
+        Log.e("hash_data", App_Data_Day3.toString());
+
+        textbox.setMovementMethod(new ScrollingMovementMethod());
+        textbox.setText(textbox.getText() + "\n\nDAY 4\n\n", TextView.BufferType.SPANNABLE);
+
+        HashMap<String, Long> App_Data_Day4 = new HashMap<String, Long>();
+        for (UsageStats usageStats : queryUsageStats4)
+
+            if (usageStats.getTotalTimeInForeground() > 60000) {  // 60000ms, = 1 minute
+                String app_parts = usageStats.getPackageName();
+                String[] split_name = app_parts.split("\\.");
+                if (!split_name[0].matches("com")) {
+                    continue;
+                }
+                String app_name = null;
+                try {
+                    app_name = split_name[split_name.length - 1];
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                App_Data_Day4.put(app_name, (usageStats.getTotalTimeInForeground() / (1000 * 60)));
+                //TextView textbox = (TextView) findViewById(R.id.editText);
+                textbox.setMovementMethod(new ScrollingMovementMethod());
+                double y = (double) usageStats.getTotalTimeInForeground();
+                DecimalFormat df = new DecimalFormat("#.##");
+                Log.e("long_int", "value" + (y / 2));
+                if ((usageStats.getTotalTimeInForeground() / (1000 * 60)) < 60) {
+                    textbox.setText(textbox.getText() + "\n" + app_name + ": "
+                            + df.format(y / (1000 * 60)) + " Minutes", TextView.BufferType.SPANNABLE);
+                } else {
+                    textbox.setText(textbox.getText() + "\n" + app_name + ": "
+                            + df.format(y / (1000 * 60 * 60)) + " Hours", TextView.BufferType.SPANNABLE);
+                }
+                Log.i("Stats", usageStats.getPackageName() + " " + usageStats.getTotalTimeInForeground());
+            }
+
+        Log.e("hash_data", App_Data_Day4.toString());
+        App_Data_Day4.keySet().retainAll(media_sites);      // ! parse out non-social media titles
+        Log.e("hash_data", App_Data_Day4.toString());
+
+        textbox.setMovementMethod(new ScrollingMovementMethod());
+        textbox.setText(textbox.getText() + "\n\nDAY 5\n\n", TextView.BufferType.SPANNABLE);
+
+        HashMap<String, Long> App_Data_Day5 = new HashMap<String, Long>();
+        for (UsageStats usageStats : queryUsageStats5)
+
+            if (usageStats.getTotalTimeInForeground() > 60000) {  // 60000ms, = 1 minute
+                String app_parts = usageStats.getPackageName();
+                String[] split_name = app_parts.split("\\.");
+                if (!split_name[0].matches("com")) {
+                    continue;
+                }
+                String app_name = null;
+                try {
+                    app_name = split_name[split_name.length - 1];
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                App_Data_Day5.put(app_name, (usageStats.getTotalTimeInForeground() / (1000 * 60)));
+                //TextView textbox = (TextView) findViewById(R.id.editText);
+                textbox.setMovementMethod(new ScrollingMovementMethod());
+                double y = (double) usageStats.getTotalTimeInForeground();
+                DecimalFormat df = new DecimalFormat("#.##");
+                Log.e("long_int", "value" + (y / 2));
+                if ((usageStats.getTotalTimeInForeground() / (1000 * 60)) < 60) {
+                    textbox.setText(textbox.getText() + "\n" + app_name + ": "
+                            + df.format(y / (1000 * 60)) + " Minutes", TextView.BufferType.SPANNABLE);
+                } else {
+                    textbox.setText(textbox.getText() + "\n" + app_name + ": "
+                            + df.format(y / (1000 * 60 * 60)) + " Hours", TextView.BufferType.SPANNABLE);
+                }
+                Log.i("Stats", usageStats.getPackageName() + " " + usageStats.getTotalTimeInForeground());
+            }
+
+        Log.e("hash_data", App_Data_Day5.toString());
+        App_Data_Day5.keySet().retainAll(media_sites);      // ! parse out non-social media titles
+        Log.e("hash_data", App_Data_Day5.toString());
+
+        textbox.setMovementMethod(new ScrollingMovementMethod());
+        textbox.setText(textbox.getText() + "\n\nDAY 6\n\n", TextView.BufferType.SPANNABLE);
+
+        HashMap<String, Long> App_Data_Day6 = new HashMap<String, Long>();
+        for (UsageStats usageStats : queryUsageStats6)
+
+            if (usageStats.getTotalTimeInForeground() > 60000) {  // 60000ms, = 1 minute
+                String app_parts = usageStats.getPackageName();
+                String[] split_name = app_parts.split("\\.");
+                if (!split_name[0].matches("com")) {
+                    continue;
+                }
+                String app_name = null;
+                try {
+                    app_name = split_name[split_name.length - 1];
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                App_Data_Day6.put(app_name, (usageStats.getTotalTimeInForeground() / (1000 * 60)));
+                //TextView textbox = (TextView) findViewById(R.id.editText);
+                textbox.setMovementMethod(new ScrollingMovementMethod());
+                double y = (double) usageStats.getTotalTimeInForeground();
+                DecimalFormat df = new DecimalFormat("#.##");
+                Log.e("long_int", "value" + (y / 2));
+                if ((usageStats.getTotalTimeInForeground() / (1000 * 60)) < 60) {
+                    textbox.setText(textbox.getText() + "\n" + app_name + ": "
+                            + df.format(y / (1000 * 60)) + " Minutes", TextView.BufferType.SPANNABLE);
+                } else {
+                    textbox.setText(textbox.getText() + "\n" + app_name + ": "
+                            + df.format(y / (1000 * 60 * 60)) + " Hours", TextView.BufferType.SPANNABLE);
+                }
+                Log.i("Stats", usageStats.getPackageName() + " " + usageStats.getTotalTimeInForeground());
+            }
+
+        Log.e("hash_data", App_Data_Day6.toString());
+        App_Data_Day6.keySet().retainAll(media_sites);      // ! parse out non-social media titles
+        Log.e("hash_data", App_Data_Day6.toString());
+
+        textbox.setMovementMethod(new ScrollingMovementMethod());
+        textbox.setText(textbox.getText() + "\n\nDAY 7\n\n", TextView.BufferType.SPANNABLE);
+
+
+
+        HashMap<String, Long> App_Data_Day7 = new HashMap<String, Long>();
+        for (UsageStats usageStats : queryUsageStats7)
+
+            if (usageStats.getTotalTimeInForeground() > 60000) {  // 60000ms, = 1 minute
+                String app_parts = usageStats.getPackageName();
+                String[] split_name = app_parts.split("\\.");
+                if (!split_name[0].matches("com")) {
+                    continue;
+                }
+                String app_name = null;
+                try {
+                    app_name = split_name[split_name.length - 1];
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                App_Data_Day7.put(app_name, (usageStats.getTotalTimeInForeground() / (1000 * 60)));
+                //TextView textbox = (TextView) findViewById(R.id.editText);
+                textbox.setMovementMethod(new ScrollingMovementMethod());
+                double y = (double) usageStats.getTotalTimeInForeground();
+                DecimalFormat df = new DecimalFormat("#.##");
+                Log.e("long_int", "value" + (y / 2));
+                if ((usageStats.getTotalTimeInForeground() / (1000 * 60)) < 60) {
+                    textbox.setText(textbox.getText() + "\n" + app_name + ": "
+                            + df.format(y / (1000 * 60)) + " Minutes", TextView.BufferType.SPANNABLE);
+                } else {
+                    textbox.setText(textbox.getText() + "\n" + app_name + ": "
+                            + df.format(y / (1000 * 60 * 60)) + " Hours", TextView.BufferType.SPANNABLE);
+                }
+                Log.i("Stats", usageStats.getPackageName() + " " + usageStats.getTotalTimeInForeground());
+            }
+
+        Log.e("hash_data", App_Data_Day7.toString());
+        App_Data_Day7.keySet().retainAll(media_sites);      // ! parse out non-social media titles
+        Log.e("hash_data", App_Data_Day7.toString());
 
     }
 
