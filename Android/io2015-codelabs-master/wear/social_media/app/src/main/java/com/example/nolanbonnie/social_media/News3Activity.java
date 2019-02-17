6ed74630220b624cf3c9df881819fb452c0c3cc4
Alvin Nguyen <realalvinnerds@gmail.com>
2019-02-17 02:01:55 -0800

    Added Scrollable Cardviews

--- Android/io2015-codelabs-master/wear/social_media/app/src/main/java/com/example/nolanbonnie/social_media/News3Activity.java
+++ Android/io2015-codelabs-master/wear/social_media/app/src/main/java/com/example/nolanbonnie/social_media/News3Activity.java
@@ -6,11 +6,18 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class News3Activity extends AppCompatActivity {
 
     private TextView mTextMessage;
+
+    private static final String TAG = "News3Activity";
+
+    private ListView mListView;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -38,10 +45,26 @@ public class News3Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news3);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setSelectedItemId(R.id.navigation_notifications);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        mListView = (ListView) findViewById(R.id.listView);
+
+        ArrayList<Card> list = new ArrayList<>();
+
+        list.add(new Card("drawable://" + R.drawable.sm, "www.google.com", "Arizona Dessert"));
+        list.add(new Card("drawable://" + R.drawable.sm, "www.google.com", "Bamf"));
+        list.add(new Card("drawable://" + R.drawable.sm, "www.google.com", "Colorado Mountains"));
+        list.add(new Card("drawable://" + R.drawable.sm, "www.google.com", "Cork"));
+        list.add(new Card("drawable://" + R.drawable.sm, "www.google.com", "DavenPort California"));
+        list.add(new Card("drawable://" + R.drawable.sm, "www.google.com", "Denmark Austrailia"));
+        list.add(new Card("drawable://" + R.drawable.sm, "www.google.com", "Foggy Iceland"));
+        list.add(new Card("drawable://" + R.drawable.sm, "www.google.com", "Havasu Falls"));
+        list.add(new Card("drawable://" + R.drawable.sm, "www.google.com", "Hawaii RainForest"));
+        list.add(new Card("drawable://" + R.drawable.sm , "www.google.com", "NewFoundLand Ice"));
+        list.add(new Card("drawable://" + R.drawable.sm, "www.google.com", "Oregon Greens"));
+        list.add(new Card("drawable://" + R.drawable.sm, "www.google.com", "Smokey Mountain"));
+        list.add(new Card("drawable://" + R.drawable.sm, "www.google.com", "Yosemite"));
+
+        CustomListAdapter adapter = new CustomListAdapter(this, R.layout.card_layout, list);
+        mListView.setAdapter(adapter);
     }
 
 }
